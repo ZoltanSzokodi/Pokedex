02f5bccfa1a31e1cfa66f6d77933f633f715ef48
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-09-16 12:20:11 +0200

    Pokecard Component, Pokedex Component and Styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,8 +1,18 @@
-import REact, { Component } from 'react';
+import React, { Component } from 'react';
+import './Pokecard.css';
+const POKE_API = "https://pokeres.bastionbot.org/images/pokemon/";
 
 class Pokecard extends Component {
   render() {
-    return <h1>POKECARD!</h1>;
+    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    return (
+      <div className="Pokecard">
+        <h1 className="Pokecard-title">{this.props.name}</h1>
+        <img className="Pokecard-img" src={imgSrc} alt={this.props.name} />
+        <div className="Pokecard-data">Type: {this.props.type}</div>
+        <div className="Pokecard-data">Exp: {this.props.exp}</div>
+      </div>
+    );
   }
 }
 
